e3aaff4397eac046b90d7c8e3cc836da2358b36a
samyside <samyside@yandex.ru>
2019-10-03 18:58:15 +0300

    add fill_random(). read_byte()

--- Tasks/task-1/program.c
+++ Tasks/task-1/program.c
@@ -3,36 +3,48 @@
 #include <stdlib.h>
 
 #define ERROR_FILE_OPEN -3
+#define FILENAME "output.db"
+
+void write_in(FILE * file, char c) {
+	fwrite(&c, sizeof(char), 1, file);
+}
+
+void fill_random(FILE * file, const int count) {
+	if (file == NULL) {
+		file = fopen(FILENAME, "wb+");
+	}
+	for (int i = 0; i < count; ++i) {
+		c = rand() % 100;
+		fwrite(&c, sizeof(char), 1, file);
+	}
+	fclose(file);
+}
+
+unsigned char read_byte(FILE * file) {
+	unsigned char c = 0;
+
+	if (file == NULL) {
+		printf("Error! Couldn't open file. Program will be closed.\n");
+		exit(ERROR_FILE_OPEN);
+	}
+	fread(&c, sizeof(char), 1, file);
+
+	return c;
+}
 
 int main(int argc, char const *argv[]) {
-	const char* FILE_BMP = "Arh02/Pic02.bmp";
-	const char* FILE_JPG = "Arh02/Pic02.jpg";
-	const char* FILE_DOC = "Arh02/Text02.doc";
-	const char* FILE_DOCX= "Arh02/Text02.docx";
-	const char* FILE_TXT = "Arh02/Text02.txt";
-
 	printf("The program has started...\n");
 
 	FILE *file_report = NULL;
-	int number_in;
-	int number_out = -1;
-	char * filename = "output.db";
-	file_report = fopen(filename, "wb+");
+	file_report = fopen(FILENAME, "wb+");
 
-	printf("number_in = ");
-	scanf("%d", &number_in);
-
-	// Запись бинарного значения в файл
-	fwrite(&number_in, sizeof(int), 1, file_report);
-	fclose(file_report);
+	// Запись в бинарный файл случайные значения
+	// fill_random(file, 150);
 
 	// Чтение и вывод бинарного значения из файла
-	file_report = fopen(filename, "rb");
-	fread(&number_out, sizeof(int), 1, file_report);
-	printf("number_out = %d", number_out);
-
-	fclose(file_report);
-
+	
+	read_byte();
+	
 	// Открыть файл как бинарный
 	// Считать количество байт в массив
 	// 1) Имя файла. Tip: константа
