3b14762477864158db6e4d0c42a2bfa759405fd5
samyside <samyside@yandex.ru>
2020-01-21 21:01:44 +0300

    check every symbol for match. and then add to the output string

--- tasks/task-5/Prog.java
+++ tasks/task-5/Prog.java
@@ -2,44 +2,61 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Prog {
-	private static final String KEY = "множитель";
-
 	public static void main(String[] args) throws Exception {
-		int N = 66;
-		final char ALPHABET[N] = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
+		final char[] ALPHABET = new String("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя").toCharArray();
+		final char[] PASS_KEY = new String("множитель").toCharArray();
 		FileReader fr = new FileReader("input.txt");
 		FileWriter fw = new FileWriter("output.txt");
 		Scanner scan = new Scanner(fr);
 
-		char bufferChar = 0;
-
-		// Получение последовательности из ключа
-		char arrayChar[] = KEY.toCharArray();
-		int keyMove[];
-		for (int i=0; i<KEY.length(); i++) {
-			for (int j=0; j<N; j++) {
-				if (KEY[i] == ALPHABET[j]) {
-					keyMove[i] = j;
+		// Получение цифрового ключа из слова
+		int pass_num[] = new int[PASS_KEY.length];
+		for (int i=0; i<PASS_KEY.length; i++) {
+			for (int j=0; j<ALPHABET.length; j++) {
+				if (PASS_KEY[i] == ALPHABET[j]) {
+					if (j > 32) pass_num[i] = j - 32;
+					else pass_num[i] = j;
 				}
 			}
 		}
 
-		// Считывание файла построчно
 		String line;
-		while(scan.hasNextLine()) {
+		String bufferLine;
+		char bufferChar = 0;
+		// Считывание файла построчно
+		while (scan.hasNextLine()) {
+			// Считываем следующую строку
 			line = scan.nextLine();
-
-			for (int i=0; i<line.length(); i++) {
-				bufferChar = line.charAt(i);
-				bufferChar += 3; // hardcoded value for movement symbol
+			// Перебираем символы из считанной строки
+			for (char bufferChar : line) {
+				// Проверяем каждый символ на соответствие
+				// символу из алфавита
+				for (char c : ALPHABET) {
+					// Если совпало, добавить в строку для 
+					// шифрования
+					if (c == bufferChar) {
+						char encryptedSymbol = encryptSymbol(bufferChar);
+						bufferLine += encryptedSymbol;
+					}
+				}
 
 				// Запись в файл зашифрованного символа
 				fw.write(bufferChar);
-				System.out.println(bufferChar);
 			}
 		}
 
 		fw.close();
 		fr.close();
+	}
+
+	// Шифрование исходного символа
+	char encryptSymbol(char symbol) {
+
+		return 0;
+	}
+
+	String doEncryptLine(String line) {
+
+		return null;
 	}
 }
